1338d774b3b76c30c7594294e60133d875fddc59
ricardaonao <ican.atende@gmail.com>
2021-07-05 05:54:19 +0000

    configura dotenv para variavel de ambiente funcionar emproducao

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  
-env: {
-  MONGO_URI: "mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority"
-}
+const createEnvFile = require('./environment-builder');
 
-}
+module.exports = createEnvFile();
+
+
+// {
+  
+// env: {
+//   MONGO_URI: "mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority"
+// }
+
+// }
