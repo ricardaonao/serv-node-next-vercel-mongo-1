418e21afe08c532c50a7305d5e1164d58db1bd19
ricardaonao <ican.atende@gmail.com>
2021-07-05 06:28:03 +0000

    teste variavel ambiente

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const createEnvFile = require('./environment-builder');
-
-module.exports = createEnvFile();
-
-
-// {
+module.exports = {
   
-// env: {
-//   MONGO_URI: "mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority"
-// }
+env: {
+  MONGO_URI: "mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority"
+}
 
-// }
+}
