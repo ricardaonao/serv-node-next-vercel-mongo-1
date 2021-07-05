2988b25cd9b1fd88168343846bb9e06d5056177b
ricardaonao <ican.atende@gmail.com>
2021-07-05 14:29:32 +0000

    configurando variavel de ambiente no next 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-module.exports = {
-  
-env: {
-  MONGO_URI: "mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority"
-}
 
-}
+module.exports = {
+    /* config options here */
+  }
