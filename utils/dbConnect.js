04afbf070a584aa80e687152b8698cfbcb4dab7b
ricardaonao <ican.atende@gmail.com>
2021-07-05 06:30:35 +0000

    teste variavel ambiente 2

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -8,7 +8,7 @@ async function dbConnect() {
     if (connection.isConnected) {
         return;
     }
-    const db = await mongoose.connect(process.env.MONGO_URI, {
+    const db = await mongoose.connect('mongodb+srv://APIs_teste:538260@cluster0.p414s.mongodb.net/APIs_teste?retryWrites=true&w=majority', {
         
         useNewUrlParser: true,
         useUnifiedTopology: true,
