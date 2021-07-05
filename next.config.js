a308ea9de039b52ecfc31dc36be8cfaf741fd3de
ricardaonao <ican.atende@gmail.com>
2021-07-05 17:09:11 +0000

    correcoes na conexao com bd feitas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    /* config options here */
+    /* crie sua variável deambiente para uso local, aqui. para uso em produção, no server, vai depender de onde você está fazendo o deploy. pesquise sobre : variáveis de embiente em produção. */
   }
